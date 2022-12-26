f69d18371371dadb1615092af919f0a9f4a3ae5e
Git-Senin <lawrencecole8@yahoo.com>
2022-12-25 17:46:45 -0800

    update project

--- scripts.js
+++ scripts.js
@@ -2,25 +2,32 @@
 class user_data{
 
     constructor(name, date, amount){
-        this.name = name
-        this.date = date
-        this.amount = amount
+        this.name = name;
+        this.date = date;
+        this.amount = amount;
     }
 }
 
 let table_data = []
 
 function addItem(){
-    table_data.push(new user_data("Jack", "2/15/2020", "$0.54"))
-}
+    const name = document.querySelector("#name").value;
+    const date = document.querySelector("#date").value;
+    const price = document.querySelector("#price").value;
 
+    const data = new user_data(name, date, price);
+    table_data.push(data);
+
+    consoleLogTable();
+}
 function consoleLogTable() {
-    console.log(`Printing Data = ${table_data.length}`)
+    console.log(`Printing Data = ${table_data.length}`);
     for(const i in table_data){
-        console.log(table_data[i])
+        console.log(table_data[i]);
     }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    document.querySelector('button').onclick = addItem
+    document.querySelector('button').onclick = addItem;
 })
+
